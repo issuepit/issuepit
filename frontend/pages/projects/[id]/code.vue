cb52817264e5bb392154efad05ea6f2368c5c770
m-s-work <122795841+m-s-work@users.noreply.github.com>
2026-03-18 10:52:18 +0100

    Update button text to 'Fetch & Pull'

--- frontend/pages/projects/[id]/code.vue
+++ frontend/pages/projects/[id]/code.vue
@@ -97,7 +97,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
             </svg>
-            {{ fetching ? 'Fetching…' : 'Fetch' }}
+            {{ fetching ? 'Fetching…' : 'Fetch & Pull' }}
           </button>
           <span v-if="store.repo.lastFetchedAt" class="text-xs text-gray-500">
             Last fetched <DateDisplay :date="store.repo.lastFetchedAt" mode="auto" />
